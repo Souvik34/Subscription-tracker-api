1900ee5135aa950f5fbbd845a1acfd6a45ac7fca
Souvik <souvik13109@gmail.com>
2025-02-26 01:21:27 +0530

    Add subscription creation and retrieval functionality with authorization

--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -1,12 +1,14 @@
 import { Router } from "express";
+import authorize from "../middlewares/auth.middleware";
+import { createSubscription, getUserSubscriptions } from "../controllers/subscription.controller";
 
 const subscriptionRouter = Router();
 subscriptionRouter.get('/',(req,res) => res.send({title:'GET all subscriptions'}))
 subscriptionRouter.get('/:id',(req,res) => res.send({title:'GET susbscription details'}))
-subscriptionRouter.post('/',(req,res) => res.send({title:'CREATE subscription'}))
+subscriptionRouter.post('/', authorize, createSubscription)
 subscriptionRouter.put('/:id',(req,res) => res.send({title:'UPDATE subscription'}))
 subscriptionRouter.delete('/:id',(req,res) => res.send({title:'DELETE subscription'}))
-subscriptionRouter.get('/:id',(req,res) => res.send({title:'GET all user subscriptions'}))
+subscriptionRouter.get('/user/:id',authorize, getUserSubscriptions)
 subscriptionRouter.put('/:id/cancel',(req,res) => res.send({title:'CANCEL subscription'}))
 subscriptionRouter.get('/upcoming-renews',(req,res) => res.send({title:'GET upcoming renewals'}))
 
